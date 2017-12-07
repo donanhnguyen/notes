446de3da4c43ee677410297b0d1a36fd2dce6ffd
Don Nguyen <thedonwind69@gmail.com>
2017-12-06 20:51:15 -0800

    notes

--- ruby_notes.rb
+++ ruby_notes.rb
@@ -1,1 +1,27 @@
 hello = "hello"
+
+class attributes
+  
+  attr_reader: can change attributes/variables but only within the class
+  
+  attr_writer: makes a change to the attribute but only outside of the class
+  
+  example, if you wanna do this:
+  
+  class Sex
+  end
+  #outside the class
+  hey = class.new
+  hey.name = "sex"
+  hey.name = "balls"
+  
+  attr_accessor: does it all
+  
+  
+  
+  !! use this before something to see if it is not-not something.
+  
+  
+  
+  
+  
